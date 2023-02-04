fb9a961aa2eef867ee946be76a9c2734fe6c4549
Ervin A <chelovek.ap@gmail.com>
2023-02-05 02:42:28 +0300

    task19

---
+++ scripts/task19.js
@@ -0,0 +1,17 @@
+//1
+let a = 10;
+console.log(a);
+
+//2
+let b = 10;
+letc = 10;
+console.log(b, c);
+
+//3
+console.log(1);
+console.log("1");
+console.log(true);
+
+//4
+const num = 0;
+num = 10; // ошибка!!!!!
