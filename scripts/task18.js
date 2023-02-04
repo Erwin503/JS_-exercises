ba4e5f8e9f6f2a2dae2e1a177e6eaf9b7b7dc4ef
Ervin A <chelovek.ap@gmail.com>
2023-02-05 02:31:10 +0300

    task18

---
+++ scripts/task18.js
@@ -0,0 +1,24 @@
+//1
+let str;
+alert(str);
+
+//2
+let ervin = null;
+alert(ervin);
+
+//3, 4
+let power = true,
+  weakness = false;
+alert(power);
+alert(weakness);
+
+//5
+let nan = "aaa",
+  test = "bbb";
+alert(nan * test);
+
+//6
+alert(10 / 0);
+
+//7
+alert(-10 / 0);
