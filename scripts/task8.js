36a63653bc2c8e34836138700a7dce53085ddb5c
Ervin A <chelovek.ap@gmail.com>
2023-02-05 01:22:13 +0300

    task8

---
+++ scripts/task8.js
@@ -0,0 +1,24 @@
+// 1
+let a = 10,
+  b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+//2
+let c = 10,
+  d = 5;
+result = c + d;
+alert(result);
+
+//3
+let a1 = 1,
+  b1 = 2,
+  c1 = 3;
+alert(a1 + b1 + c1);
+
+//4
+let a2 = 10,
+  b2 = 5;
+let c2 = a2 - b2;
