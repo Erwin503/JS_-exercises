b5bd9faf620a3515e662a96f07b5b5c42fe223a6
Ervin A <chelovek.ap@gmail.com>
2023-02-05 02:01:04 +0300

    task15

---
+++ scripts/task15.js
@@ -0,0 +1,13 @@
+//1
+let str = "!!!";
+alert(str);
+
+//2
+let java = "java",
+  script = "script";
+alert(java + script);
+
+//3
+let hello = "hello",
+  world = "world";
+alert(hello + " " + world);
