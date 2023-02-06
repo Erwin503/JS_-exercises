761d13287223238ed1f77fa8335141bd569c46b2
Ervin A <chelovek.ap@gmail.com>
2023-02-06 10:11:59 +0300

    task23

---
+++ scripts/task23.js
@@ -0,0 +1,14 @@
+//1
+let a = "5px";
+let b = "6px";
+alert(parseInt(a) + parseInt(b));
+
+//2
+let c = parseFloat("5.5px");
+let d = parseFloat("6.25px");
+alert(c + d);
+
+//3
+let e = parseFloat("5.5px");
+let f = parseFloat("6.25px");
+alert(e + f + "px");
