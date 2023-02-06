93240a6615ee71ec830123ad93ec1bbca3c05677
Ervin A <chelovek.ap@gmail.com>
2023-02-06 10:16:44 +0300

    task24

---
+++ scripts/task24.js
@@ -0,0 +1,13 @@
+//1
+let a = 1;
+let b = 2;
+alert(String(a) + " " + String(b));
+
+//2
+let c = 133;
+alert(String(c).length);
+
+//3
+let d = 133;
+let e = 1334;
+alert(String(d).length + String(e).length);
