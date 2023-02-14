0ea90d06df173094834c72d2cafd1b6adcbc6b36
Ervin A <chelovek.ap@gmail.com>
2023-02-14 19:56:59 +0300

    task33

---
+++ scripts/task33.js
@@ -0,0 +1,13 @@
+// 1
+// alert("Ваш возраст: " + prompt("Ваш возраст?"));
+// 2
+// let num1 = Number(prompt("Введите первое число"));
+// let num2 = Number(prompt("Введите второе число"));
+// alert(num1 + num2);
+// 3
+// let num = Number(prompt("Введите число"));
+// alert(num ** 2);
+// 4
+let num1 = Number(prompt("Введите первое число"));
+let num2 = Number(prompt("Введите второе число"));
+alert(num1 * num2);
