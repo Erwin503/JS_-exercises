90ea172fc30f44dd7cd74a7ee530b5485f9454b6
Ervin A <chelovek.ap@gmail.com>
2023-02-14 22:05:42 +0300

    task41

---
+++ scripts/task41.js
@@ -0,0 +1,18 @@
+// 1
+// let arr = ["a", "b", "c"];
+// arr[0] = "1";
+// arr[0] = "2";
+// arr[0] = "3";
+// console.log(arr);
+// 2
+// let arr = [1, 2, 3];
+// for (i in arr) {
+//     arr[i] = arr[i] + 3
+// }
+// console.log(arr);
+// 3
+// let arr = [1, 2, 3];
+// for (i in arr) {
+//     arr[i]++;
+// }
+// console.log(arr);
