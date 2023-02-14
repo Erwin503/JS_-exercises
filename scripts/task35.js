73f7658bad9c0b3a21134b42611ef1f4786419b2
Ervin A <chelovek.ap@gmail.com>
2023-02-14 21:39:16 +0300

    task35

---
+++ scripts/task35.js
@@ -0,0 +1,53 @@
+// 1
+// let num1 = 1;
+// let num2 = 2;
+// sum = num1 + num2;
+// console.log("сумма: " + sum);
+// 2
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
+// 3
+// let num = '123';
+// let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+// console.log(sum);
+//4
+// let num = '123';
+// console.log(num[0]);
+// 5
+// let a = 0;
+// console.log(++a);
+// 6
+// let num = '123';
+// console.log(num.length);
+// 7
+// let a = 24 * 60 * 60;
+// console.log(a);
+// 8
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+// 9
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length - 1]);
+// 10
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+// 11
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length - 1]);
+// 12
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + Number(b);
+
+// console.log(s);
+// 13
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+
+// console.log(aaa + bbb + ccc);
