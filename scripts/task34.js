a09eca3ce7801efdf0380eccb8e576854e0a4e26
Ervin A <chelovek.ap@gmail.com>
2023-02-14 20:11:10 +0300

    task34

---
+++ scripts/task34.js
@@ -0,0 +1,9 @@
+// 1
+document.write("ERVIN");
+// 2
+document.write("<i>ERVIN<i>");
+// 3
+let str = "Ervin";
+document.write("<i>" + str + "</i>");
+// 4
+document.write("<ul><li>1<li><li>2<li><li>3<li><li>4<li><li>5<li><ul>");
