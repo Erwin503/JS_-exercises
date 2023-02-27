a9057f5cd11f794611549901b248b82f086e07ea
Ervin A <chelovek.ap@gmail.com>
2023-02-27 20:59:52 +0300

    task107

---
+++ scripts/task107.js
@@ -0,0 +1,15 @@
+// 1
+let num = 120;
+
+if (num % 2 == 0) {
+	console.log('Четное число');
+} else {
+	console.log('Нечетное число');
+}
+
+// 2
+if (num % 3 == 0) {
+	console.log('Число кратно 3');
+} else {
+	console.log('Число не кратно 3');
+}
