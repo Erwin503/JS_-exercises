fb44b1760eeda4aba680a936c340a7d959cccd8e
Ervin A <chelovek.ap@gmail.com>
2023-02-27 20:46:38 +0300

    task95

---
+++ scripts/task95.js
@@ -0,0 +1,12 @@
+let num = 77;
+
+if (num >= 10 && num <= 99) {
+  new_num = String(num);
+  if (Number(new_num[0]) + Number(new_num[1]) <= 9) {
+    console.log("Cумма цифр однозначна");
+  } else {
+    console.log("Cумма цифр двухзначна");
+  }
+} else {
+  console.log("Неверное значение");
+}
