e43a3c5e75ab8b0f49b2d8288ac38169f906ccca
Ervin A <chelovek.ap@gmail.com>
2023-02-27 21:01:07 +0300

    task108

---
+++ scripts/task108.js
@@ -0,0 +1,124 @@
+// 1;
+// let num1 = 1;
+// let num2 = 2;
+
+// if (num1 + num2 === 3) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 2;
+// let num1 = "1";
+// let num2 = "2";
+
+// if (+num1 + +num2 === 3) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 3;
+// let num1 = "1";
+// let num2 = "2";
+
+// if (Number(num1) + Number(num2) === 3) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 4;
+// let num = 123;
+
+// if (String(num)[0] === "1") {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 5;
+// let num = 123;
+
+// if (String(num)[0] === "1") {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 6;
+// let num = 123;
+
+// if (String(num)[0] === "1") {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 7;
+// let num = 123;
+// let first = String(num)[0];
+
+// if (first === "1") {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 8;
+// let num = 12;
+
+// if (String(num).length === 2) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 9;
+// let num = 12;
+// let str = String(num);
+
+// if (str.length === 2) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 10;
+// let num = 12;
+
+// if (String(num).length == 2) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 11;
+// let num = 12;
+
+// if (String(num).length === 2) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 12;
+// let num = 12;
+
+// if (String(num).length === 2) {
+//   console.log("+++");
+// } else {
+//   console.log("---");
+// }
+
+// 13;
+// let num = "123033"; // берем в кавычки, чтобы обращаться к цифрам
+
+// let sum1 = +num[0] + +num[1] + +num[2];
+// let sum2 = +num[3] + +num[4] + +num[5];
+
+// if (sum1 == sum2) {
+//   console.log("суммы равны");
+// } else {
+//   console.log("суммы не равны");
+// }
