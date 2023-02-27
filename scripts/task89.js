9aa21970f2d9ded263fc997587ac2b9c75298304
Ervin A <chelovek.ap@gmail.com>
2023-02-27 20:38:32 +0300

    task89

---
+++ scripts/task89.js
@@ -0,0 +1,26 @@
+// 1
+let test1 = true;
+
+if (!test1) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+// 2
+let test2 = true;
+
+if (!test2) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
+
+// 3
+let test3 = true;
+
+if (test3) {
+  console.log("+++");
+} else {
+  console.log("---");
+}
