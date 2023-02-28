b11ceb3e0b2accc1f93f949eb3183dc4b3b8893a
Ervin A <chelovek.ap@gmail.com>
2023-02-28 19:46:01 +0300

    task111

---
+++ scripts/task111.js
@@ -0,0 +1,34 @@
+
+// 1
+console.log('1');
+for (let i = 1; i <= 100; i++) {
+	console.log(i);
+}
+
+// 2
+console.log('2');
+for (let i = 11; i <= 33; i++) {
+	console.log(i);
+}
+
+// 3
+console.log('3');
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// 4
+console.log('4');
+for (let i = 1; i <= 99; i++) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+}
+
+// 5
+console.log('5');
+for (let i = 100; i >= 0; i--) {
+    console.log(i);
+}
